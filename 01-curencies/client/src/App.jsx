8deab5dbd5248ff55fd03f18d8180a2360f876f3
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-02-21 23:44:38 -0500

    getting data properly

--- 01-curencies/client/src/App.jsx
+++ 01-curencies/client/src/App.jsx
@@ -1,43 +1,103 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-import "./App.css";
+import CURRENCIES from "./symbols/currencySymbols";
 
 function App() {
     const [convertFrom, setConvertFrom] = useState("EUR");
     const [convertTo, setConvertTo] = useState("USD");
     const [amount, setAmount] = useState(1);
+    const [data, setData] = useState({});
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.get(
+                "https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert",
+                {
+                    params: {
+                        from: convertFrom,
+                        to: convertTo,
+                        amount,
+                    },
+                    headers: {
+                        "X-RapidAPI-Key":
+                            "d8170df1a3msh7787fba2e6d12ecp17db82jsnd33802ecc2b6",
+                        "X-RapidAPI-Host":
+                            "currency-conversion-and-exchange-rates.p.rapidapi.com",
+                    },
+                }
+            );
+            setData(response);
+        } catch (error) {
+            console.error(error);
+        }
+
+        console.log("Convert From:", convertFrom);
+        console.log("Convert To:", convertTo);
+        console.log("Amount:", amount);
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            const options = {
-                method: "GET",
-                url: "https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert",
-                params: {
-                    from: convertFrom,
-                    to: convertTo,
-                    amount,
-                },
-                headers: {
-                    "X-RapidAPI-Key":
-                        "d8170df1a3msh7787fba2e6d12ecp17db82jsnd33802ecc2b6",
-                    "X-RapidAPI-Host":
-                        "currency-conversion-and-exchange-rates.p.rapidapi.com",
-                },
-            };
+        console.log("DATA:", data);
+    }, [data]);
 
-            try {
-                const response = await axios.request(options);
-                console.log(response.data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    return <></>;
+    return (
+        <>
+            <div className="flex items-center justify-center h-screen">
+                <div>
+                    <p className="">Converter</p>
+                    <form
+                        className="flex flex-row gap-2 items-center"
+                        onSubmit={handleSubmit}
+                    >
+                        <input
+                            className="p-2 border rounded-md bg-slate-50"
+                            type="number"
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.value)}
+                        />
+                        <select
+                            className="bg-slate-600 text-stone-200 text-md rounded-md max-w-[256px] px-2 py-3"
+                            value={convertFrom}
+                            onChange={(e) => setConvertFrom(e.target.value)}
+                        >
+                            {CURRENCIES.map((symbol) => (
+                                <option
+                                    key={symbol.symbol}
+                                    value={symbol.symbol}
+                                >
+                                    {symbol.symbol}
+                                </option>
+                            ))}
+                        </select>
+                        <p className="text-lg">to</p>
+                        <select
+                            className="bg-slate-600 text-stone-200 text-md rounded-md max-w-[256px] px-3 py-3"
+                            value={convertTo}
+                            onChange={(e) => setConvertFrom(e.target.value)}
+                        >
+                            {CURRENCIES.map((symbol) => (
+                                <option
+                                    key={symbol.symbol}
+                                    value={symbol.symbol}
+                                >
+                                    {symbol.symbol}
+                                </option>
+                            ))}
+                        </select>
+                        <button
+                            type="submit"
+                            className="bg-slate-200 border-slate-600 border text-slate-600 py-3 px-4 rounded-md mx-2"
+                        >
+                            Convert
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ 02-currencies/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function App() {
+    return (
+        <>
+            <h1 className="text-red-600">Test</h1>
+        </>
+    );
+}
+
+export default App;
