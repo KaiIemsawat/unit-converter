17da5b66c7b35ef38525b29e78a33847612ce947
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-02-21 21:15:46 -0500

    redo all / api results work

--- 01-curencies/client/src/App.jsx
+++ 01-curencies/client/src/App.jsx
@@ -1,17 +1,43 @@
 import axios from "axios";
-import { useState } from "react";
-import CurrrentRow from "./components/CurrrentRow";
+import { useState, useEffect } from "react";
+
 import "./App.css";
 
 function App() {
-    return (
-        <>
-            <h1>Converter</h1>
-            <CurrrentRow />
-            <div className="equals">=</div>
-            <CurrrentRow />
-        </>
-    );
+    const [convertFrom, setConvertFrom] = useState("EUR");
+    const [convertTo, setConvertTo] = useState("USD");
+    const [amount, setAmount] = useState(1);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const options = {
+                method: "GET",
+                url: "https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert",
+                params: {
+                    from: convertFrom,
+                    to: convertTo,
+                    amount,
+                },
+                headers: {
+                    "X-RapidAPI-Key":
+                        "d8170df1a3msh7787fba2e6d12ecp17db82jsnd33802ecc2b6",
+                    "X-RapidAPI-Host":
+                        "currency-conversion-and-exchange-rates.p.rapidapi.com",
+                },
+            };
+
+            try {
+                const response = await axios.request(options);
+                console.log(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return <></>;
 }
 
 export default App;
